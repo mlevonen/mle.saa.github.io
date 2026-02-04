497d19a9f81baf90c55af07fa8cfdf141e33243a
mlevonen <65166184+mlevonen@users.noreply.github.com>
2026-02-04 18:15:08 +0200

    legendat23

--- main.js
+++ main.js
@@ -829,7 +829,7 @@ plugins: {
           // 🔴 Puuskat täytetään kokonaan
           if (label.text.includes("Puuska")) {
             label.fillStyle = label.strokeStyle;
-            label.lineWidth = 0;          // 🔑 estää viivan
+            label.lineWidth = 5;          // 🔑 estää viivan
           }
         });
 
