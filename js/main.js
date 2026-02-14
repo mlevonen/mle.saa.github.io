45b4935fb95ae55859af5e17944e47bbafdcaefb
mlevonen <marko.levonen@gmail.com>
2026-02-14 17:00:17 +0200

    isot tuulimuutokset 1

--- js/main.js
+++ js/main.js
@@ -459,7 +459,7 @@ function createWindIcon(speed, direction) {
 
   const svg = `
     <svg width="36" height="36" viewBox="0 0 24 24"
-         style="transform: rotate(${direction}deg);">
+         style="transform: rotate(${direction - 180}deg);">
       <path d="M12 2 L12 16 M6 10 L12 16 L18 10"
             stroke="#111"
             stroke-width="2.5"
