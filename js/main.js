34275d707328c1ef7d6cf62d87e759235b227d6a
mlevonen <marko.levonen@gmail.com>
2026-02-16 17:33:33 +0200

    isot tuulimuutokset 62

--- js/main.js
+++ js/main.js
@@ -483,10 +483,10 @@ function getMarkerStyle(type) {
   }
 }
 
-//CREATEWINDICON
+//CREATEWINDICON const rotation = (direction + 180) % 360;
 function createWindIcon(speed, direction) {
 
-  const rotation = (direction + 180) % 360;
+  const rotation = 0;
 
   const svg = `
     <svg width="36" height="36" viewBox="0 0 24 24"
