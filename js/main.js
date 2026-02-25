d0baf39790af27b237b82ebb08c72f859d7090d3
mlevonen <marko.levonen@gmail.com>
2026-02-25 14:47:38 +0200

    Sealevel 9

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import { renderTemperatureChart } from "./charts/temperatureChart.js";
 import { renderWindCharts } from "./charts/windChart.js";
 import { renderPopupExtras } from "./popup/popupExtras.js";
 import "./charts/plugins.js";
-import { fetchSeaLevelSeries } from ".api/sealevel.js";
+import { fetchSeaLevelSeries } from "./api/sealevel.js";
 
 
 "use strict";
