6307afab82be00fdae1aa58e9c36e968bf56db31
mlevonen <marko.levonen@gmail.com>
2026-02-28 18:01:40 +0200

    Sealevel otsikko 5

--- js/main.js
+++ js/main.js
@@ -623,7 +623,7 @@ async function renderSeaLevelPopup(popup, station) {
       margin-bottom:14px;
     ">
     <span style="display:flex; align-items:center; gap:6px;">
-      <img src="/js/assets/icons/sealevel.svg"
+      <img src="/js/assets/icons/sea-level.svg"
           style="width:18px; height:18px;">
       ${latestLevel !== null ? latestLevel + " cm" : "–"}
     </span>
