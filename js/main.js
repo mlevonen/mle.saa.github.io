8700a1af4230bc1da8777a05cfb5261aa6de2e4a
mlevonen <marko.levonen@gmail.com>
2026-03-03 12:38:40 +0200

    Pallot 3

--- js/main.js
+++ js/main.js
@@ -500,10 +500,10 @@ function createWindIcon(speed, direction) {
 
   // 🎨 Väri nopeuden mukaan
   const color =
-  roundedSpeed < 5  ? "rgba(76,175,80,0.85)" :
-  roundedSpeed < 10 ? "rgba(255,193,7,0.85)" :
-  roundedSpeed < 15 ? "rgba(255,152,0,0.85)" :
-                      "rgba(244,67,54,0.85)";
+  roundedSpeed < 5  ? "rgba(76,175,80,0.45)" :
+  roundedSpeed < 10 ? "rgba(255,193,7,0.45)" :
+  roundedSpeed < 15 ? "rgba(255,152,0,0.45)" :
+                      "rgba(244,67,54,0.45)";
 
   return L.divIcon({
     className: "wind-wrapper",
