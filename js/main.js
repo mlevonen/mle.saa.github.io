c0076dbab7449e9fe83de96461e2f8ba8253d03e
mlevonen <marko.levonen@gmail.com>
2026-03-03 12:28:59 +0200

    Pallot 1

--- js/main.js
+++ js/main.js
@@ -510,7 +510,7 @@ function createWindIcon(speed, direction) {
     html: `
       <div class="wind-marker" style="background:${color};">
         <div class="wind-arrow"
-             style="transform: rotate(${direction}deg);">
+             style="transform: rotate(${(direction + 180) % 360}deg);">
           ▲
         </div>
         <div class="wind-speed">
