dae8b14309306954032f314a12838070feeb483b
mlevonen <marko.levonen@gmail.com>
2026-03-03 12:55:59 +0200

    Pallot 8

--- js/main.js
+++ js/main.js
@@ -529,8 +529,8 @@ function createWindIcon(speed, direction) {
         </div>
       </div>
       `,
-    iconSize: [54, 54],
-    iconAnchor: [27, 27]
+    iconSize: [46, 46],
+    iconAnchor: [23, 23]
   });
 }
 
