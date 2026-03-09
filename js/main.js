f4c1f1e1300aad5fc5308cc83d60bd3c42461b6b
mlevonen <marko.levonen@gmail.com>
2026-03-09 17:14:06 +0200

    Layer muutoksia 1

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ const FMI_WFS = "https://opendata.fmi.fi/wfs";
 
 
 const weatherLayer = L.featureGroup().addTo(map);
-const seaLevelLayer = L.featureGroup().addTo(map);
+//const seaLevelLayer = L.featureGroup().addTo(map);
 const coastalLayer = L.featureGroup().addTo(map);
 
 const markerRegistry = {};
