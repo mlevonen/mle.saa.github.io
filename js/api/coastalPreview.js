ccc362f6b9b9eafde4fd5c300f76abe3e0b41180
mlevonen <marko.levonen@gmail.com>
2026-03-12 16:48:42 +0200

    Markkerit uusiksi 50

--- js/api/coastalPreview.js
+++ js/api/coastalPreview.js
@@ -1,6 +1,5 @@
 import { smartSymbolIcon } from "../popup/popupExtras.js";
 import { fetchObservationSeriesByFmisid } from "./dataLoader.js";
-import { getSmartSymbol } from "../popup/popupExtras.js";
 
 // ==========================
 // COASTAL PREVIEW SYSTEM
@@ -35,7 +34,6 @@ function loadCache() {
 
 }
 
-
 function saveCache(values) {
 
   localStorage.setItem(
@@ -47,7 +45,6 @@ function saveCache(values) {
   );
 
 }
-
 
 async function fetchCoastalMulti(fmisids) {
 
@@ -191,51 +188,6 @@ export async function updateCoastalPreview(
   console.log("VALUES OBJECT:", values);
 
   updateMarkers(values, markerRegistry, createWindIcon);
-
-  saveCache(values);
-
-}
-
-
-{
-  const values = {};
-
-  const coastalStations = stations.filter(
-    s => s.type === "coastal" && s.featured
-  );
-
-  const requests = coastalStations.map(async station => {
-
-    try {
-
-      const data = await loadPopupData({
-        lat: station.lat,
-        lon: station.lon,
-        weatherPlace: null,
-        weatherFmisid: station.fmisid,
-        seaLevelFmisid: null
-      });
-
-      const latestWind = data.obsWindSpeed?.at(-1);
-
-      if (!latestWind) return;
-
-      values[station.fmisid] = {
-        wind: latestWind.windspeedms,
-        dir: latestWind.winddirection,
-        symbol: latestWind.smartsymbol
-      };
-
-      console.log("COASTAL SYMBOL:", station.name, latestWind.smartsymbol);
-
-
-
-    } catch {}
-
-  });
-
-  await Promise.all(requests);
-
 
   saveCache(values);
 
