2da4cdad0ca39a900f19c2502a02346b0404558a
mlevonen <marko.levonen@gmail.com>
2026-03-15 19:13:40 +0200

    Sääsymbolit graafiin 21

--- js/charts/plugins/weatherSymbols.js
+++ js/charts/plugins/weatherSymbols.js
@@ -26,22 +26,31 @@ export const weatherSymbolsPlugin = {
 
     const { ctx } = chart;
 
+    const ICON_SIZE = 26;
+    const ICON_OFFSET = 8; // etäisyys chartin yläreunasta
+
     dataset.data.forEach((point, i) => {
 
-    if (!point.symbol) return;
+      if (!point.symbol) return;
 
-    // näytä symboli vain 3h välein
-    if (point.x.getUTCHours() % 3 !== 0) return;
+      // näytä vain 3h välein
+      if (point.x.getUTCHours() % 3 !== 0) return;
 
-    const pos = meta.data[i].getProps(["x"], true);
+      const pos = meta.data[i].getProps(["x"], true);
 
-    const img = getIcon(point.symbol);
+      const img = getIcon(point.symbol);
 
-    const y = chart.chartArea.top + 6;
+      const y = chart.chartArea.top - ICON_SIZE - ICON_OFFSET;
 
-    if (img.complete && img.naturalWidth > 0) {
-    ctx.drawImage(img, pos.x - 12, y, 26, 26);
-    }
+      if (img.complete && img.naturalWidth > 0) {
+        ctx.drawImage(
+          img,
+          pos.x - ICON_SIZE / 2,
+          y,
+          ICON_SIZE,
+          ICON_SIZE
+        );
+      }
 
     });
 
