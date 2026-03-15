d910ed4ba7cfa49b7f5e169da4dccb557a4de911
mlevonen <marko.levonen@gmail.com>
2026-03-15 19:45:25 +0200

    Sääsymbolit markkeriin 9

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ import { fetchSeaLevel, fetchSeaLevelSeries, fetchSeaLevelForecast } from "./api
 import { fetchSeaLevelMulti } from "./api/sealevel.js";
 import { updateCoastalPreview, loadCoastalPreviewCache} from "./api/coastalPreview.js";
 import { updateWeatherPreview } from "./api/weatherPreview.js";
-import { getCurrentSymbol } from "../charts/utils/weatherSymbols.js";
+import { getCurrentSymbol } from "../charts/plugins/weatherSymbols.js";
 
 
 "use strict";
