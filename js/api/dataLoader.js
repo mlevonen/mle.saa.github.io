f607bf86e36d979d619b317fc46f46c7b119c274
mlevonen <marko.levonen@gmail.com>
2026-03-18 19:01:55 +0200

    Sääsymbolit markkeriin 46

--- js/api/dataLoader.js
+++ js/api/dataLoader.js
@@ -121,7 +121,7 @@ export async function loadPopupData({
   const fcWindGust = harmonie?.fcWindGust ?? [];
 console.log("SMART SYMBOLS:", smartSymbols?.slice(0,5));
 console.log("symbolNow:", symbolNow);
-
+console.log("FIRST SYMBOL OBJECT:", smartSymbols[0]);
   // ==========================
   // VEDENKORKEUS
   // ==========================
